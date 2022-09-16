be9b472dc5fe092720c40f06f0cdca579e84e8e1
Luana <luanaprm@gmail.com>
2022-09-16 18:34:28 +0200

    Lab 2_08

--- lab_sql_2_8.sql
+++ lab_sql_2_8.sql
@@ -2,7 +2,7 @@ USE sakila;
 
 -- 1.	Write a query to display for each store its store ID, city, and country.
 
-SELECT a.store_id, city.city, coun.country
+SELECT s.store_id, city.city, coun.country
 FROM sakila.store s
 JOIN sakila.address a
 ON s.address_id=a.address_id
@@ -12,11 +12,13 @@ JOIN sakila.country coun
 ON city.country_id = coun.country_id;
 
 -- 2.	Write a query to display how much business, in dollars, each store brought in.
-SELECT SUM(amount)
+SELECT SUM(amount), c.store_id
 FROM sakila.payment a
 JOIN sakila.staff b
 ON a.staff_id = b.staff_id
-GROUP BY a.staff_id;
+JOIN sakila.store c
+USING (store_id)
+GROUP BY c.store_id;
 
 -- 3.	Which film categories are longest?
 SELECT AVG(length), c.name
@@ -58,10 +60,10 @@ WHERE f.title = "Academy Dinosaur" AND store_id=1;
 
 SELECT * from film_actor;
 
-SELECT f_a1.actor_id, CONCAT(a1.first_name,' ', a1.last_name), f_a2.actor_id, CONCAT(a2.first_name,' ', a2.last_name), f.title
+SELECT *
 FROM sakila.film_actor f_a1
 JOIN sakila.actor a1 USING (actor_id)
-JOIN sakila.film_actor f_a2
+INNER JOIN sakila.film_actor f_a2
 ON (f_a1.actor_id <> f_a2.actor_id) AND (f_a1.film_id = f_a2.film_id)
 JOIN sakila.actor a2
 ON a2.actor_id=f_a2.actor_id
@@ -69,9 +71,41 @@ JOIN sakila.film f
 ON f.film_id=f_a2.film_id
 ORDER BY f.title;
 
+SELECT 
+f_a1.actor_id, CONCAT(a1.first_name,' ', a1.last_name) AS "first_actor",
+f_a2.actor_id, CONCAT(a2.first_name,' ', a2.last_name) AS "second_actor",
+f.title
+FROM sakila.film_actor f_a1
+JOIN sakila.film_actor f_a2
+ON (f_a1.actor_id <> f_a2.actor_id) AND (f_a1.film_id = f_a2.film_id)
+JOIN sakila.actor a1
+ON a1.actor_id=f_a1.actor_id
+JOIN sakila.actor a2 
+ON a2.actor_id=f_a2.actor_id
+JOIN sakila.film f
+ON f.film_id=f_a2.film_id
+WHERE (f_a1.actor_id > f_a2.actor_id)
+ORDER BY f.title;
+
 
 -- 8.	Get all pairs of customers that have rented the same film more than 3 times.
--- BONUS
+-- I cant count the pairs =(
+SELECT r1.rental_id, f.title,
+CONCAT(c.first_name," ",c.last_name) AS "customer_1",
+CONCAT(c2.first_name," ",c2.last_name) AS "customer_2",
+f2.title, r2.rental_id,
+concat(c.customer_id,c2.customer_id) AS "pair"
+FROM sakila.inventory i
+JOIN sakila.film f USING (film_id)
+JOIN sakila.rental r1 USING (inventory_id)
+JOIN sakila.customer c USING (customer_id)
+JOIN sakila.rental r2
+ON (r1.customer_id <> r2.customer_id)
+RIGHT JOIN sakila.customer c2 ON (r2.customer_id)
+JOIN film f2 USING (film_id)
+WHERE f.title = f2.title AND r1.customer_id > r2.customer_id
+LIMIT 20000; -- limit when testing to avoind time out
+
 
 -- 9.	For each film, list actor that has acted in more films.
 -- BONUS
